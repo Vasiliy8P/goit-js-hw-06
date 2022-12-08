1b851d5a90817a63a1a3f0448c9466aa5cfc99af
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2022-12-08 17:35:42 +0200

    Task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+// const makeAllIngredients = ingredients.map(ingredient => {
+//   const positionIngredient = document.createElement("li");
+//   positionIngredient.textContent = ingredient;
+//   positionIngredient.classList.add("item");
+//   return positionIngredient;
+// })
+// ingredientsEl.append(...makeAllIngredients);
+
+const makeAllIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const positionIngredient = document.createElement("li");
+    positionIngredient.textContent = ingredient;
+    positionIngredient.classList.add("item");
+    return positionIngredient;
+  })
+}
+const elements = makeAllIngredients(ingredients);
+
+ingredientsEl.append(...elements);
+
+console.log(ingredientsEl);
+
