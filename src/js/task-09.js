812609ebadc1385be4c464925ce88d5b7713de78
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2022-12-11 18:55:23 +0200

    Task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const buttonChangeColorEl = document.querySelector('.change-color');
+const spanColorEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+buttonChangeColorEl.addEventListener('click', onChangeColor)
+
+function onChangeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanColorEl.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
