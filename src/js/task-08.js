b6c186149a357f270c8fa26ac5aad58a1c118ce0
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2022-12-11 13:08:45 +0200

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    if (!email.value || !password.value) {
+        alert("Всі поля повинні бути заповнені");
+    } else {
+        const submitObj = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(submitObj);
+        event.currentTarget.reset()
+    }    
+}
