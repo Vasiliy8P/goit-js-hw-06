be50de809126ef1b796de00825260646a4e21c6e
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2022-12-12 02:22:29 +0200

    Task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,35 @@
+const controlsEl = document.querySelector('#controls input');
+const buttonCreateEl = document.querySelector('button[data-create]')
+const buttonDestroyEl = document.querySelector('button[data-destroy]')
+const boxesEl = document.querySelector('#boxes')
+
+buttonCreateEl.addEventListener('click', onElementCreate);
+buttonDestroyEl.addEventListener('click', onElementDestroy)
+
+function onElementCreate(event) {
+  createBoxes(controlsEl.value);  
+}
+ 
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+  let elements = `<div style="background-color: ${getRandomHexColor()};  display: flex;" width="${width}" height="${height}"> </div>`;
+  
+  for (let i = 1; i < amount; i += 1) {
+    elements += `<div style="background-color: ${getRandomHexColor()}; display: flex;" width="${width += 10}" height="${height += 10}"> </div>`;
+  }
+  boxesEl.insertAdjacentHTML('beforeend', elements);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onElementDestroy(event) {
+  destroyBoxes()
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  controlsEl.value = '';
 }
